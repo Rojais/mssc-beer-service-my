4a89909986777a23c27c7462aa02649dfeaa213b
Rohit Jaiswal <rojais.jaiswal@gmail.com>
2022-05-06 17:54:26 +0530

    Add Spring Data JPA Repository for Beer object #6

---
+++ src/main/java/guru/springframework/msscbeerservicemy/repositories/BeerRepository.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscbeerservicemy.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import guru.springframework.msscbeerservicemy.domain.Beer;
+
+/**
+ * Created by jt on 2019-05-17.
+ */
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
